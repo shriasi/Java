63bcdf55a063d49fe11618ca46bbb91eebea176a
Shrimali Senevirathna <42972697+shriasi@users.noreply.github.com>
2020-03-22 04:14:42 +0530

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,10 @@
+/*Exercise 1
+      Write a program to input marks of three subjects for a student and calculate the average marks.
+      In your class.
+      a) Include a constructor to initialize the three marks to 0
+      b) Include a method to calculate and store the average
+      c) Include a method to display the ID, name and the average marks of the student
+  */
+  
+  
+ 
