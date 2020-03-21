9d889e67627da95cea420c8106060cda8b6806ff
Shrimali Senevirathna <42972697+shriasi@users.noreply.github.com>
2020-03-22 04:17:36 +0530

    Update

--- Exercise2.java
+++ Exercise2.java
@@ -1,1 +1,11 @@
+/*Exercise 2
+Write a program to represent a class called Point3D that stores the coordinates of a point in 3D
+space. Add a constructor to initialize the variables passed. Add a method called distance () to
+calculate the distance from origin to a point.
+Create another class called PointApp with the main function and create two objects of the class
+Point3D. Calculate and display the distance to the origin of these objects.
+Note: distance = sqrt( x
+2 +y2 + z2
+)
+*/
 
