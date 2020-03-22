88ee2500f6b3c2e0a0c79eb744411a4864b5eda8
Shrimali Senevirathna <42972697+shriasi@users.noreply.github.com>
2020-03-22 13:55:56 +0530

    Update

--- Exercise2.java
+++ Exercise2.java
@@ -9,3 +9,34 @@ Note: distance = sqrt( x
 )
 */
 
+// Java program to create a point 3D  
+// object and display its coordinates 
+// and find its distance from origin 
+import javafx.geometry.Point3D; 
+  
+public class Point3D_1 { 
+  
+    // Main Method 
+    public static void main(String args[]) 
+    { 
+  
+        // Create a point3D object 
+        Point3D point3d_1 = new Point3D(20.0f, 50.0f, 70.0f); 
+  
+        double x, y, z; 
+  
+        // get the coordinates of the point 
+        x = point3d_1.getX(); 
+        y = point3d_1.getY(); 
+        z = point3d_1.getZ(); 
+  
+        // display the coordinates of the point 
+        System.out.println("x coordinate = " + x  
+                            + ", y coordinate = " 
+                            + y + ", z coordinate = " + z); 
+  
+        // print its distance from origin 
+        System.out.println("Distance From Origin = " 
+                     + point3d_1.distance(0, 0, 0)); 
+    } 
+} 
