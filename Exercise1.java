beafc567286d501098098bfb36cd9c8dff2c3482
Shrimali Senevirathna <42972697+shriasi@users.noreply.github.com>
2020-03-22 13:52:23 +0530

    Update

--- Exercise1.java
+++ Exercise1.java
@@ -6,5 +6,40 @@
       c) Include a method to display the ID, name and the average marks of the student
   */
   
-  
+package exercise1;
+
+public class Marks {
+    
+	
+        float mark1 , mark2 , mark3 , Avg;
+        String name , Sid;
+        
+       public Marks(int mark1 , int mark2 , int mark3) {
+    	   
+    	   this.mark1 =0;
+    	   this.mark2 = 0;
+    	   this.mark3 = 0;
+    	 
+       }
+       void Calculate() {
+    	  
+    	   this.Avg =( (this.mark1 +this.mark2 + this.mark3)/3);
+    	   this.name = "Nimal";
+    	   this.Sid = "0002";
+    	   
+    	      System.out.println("Student Details are\n") ;
+        	System.out.println("student name is:"+ this.name ) ;
+        	System.out.println("student ID is:"+ this.Sid ) ;
+        	System.out.println("students avergae mark is :" + this.Avg);
+    	    
+       }
+       public static void main(String[] args) {
+   		// TODO Auto-generated method stub
+   		
+        	Marks m1 = new Marks( 10 , 10 ,10);
+        	m1.Calculate();
+   	}
+       
+}
+   
  
